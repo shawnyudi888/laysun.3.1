cf2fdeafb46bd8accfc42a1e637fb11e3ae1f13d
shawnyudi888 <9962463yudi@gmail.com>
2026-03-04 22:02:18 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-  },
+  publicDir: 'static',  // 告诉 Vite 使用 static 作为公共目录
+  // ... 其他配置
 })

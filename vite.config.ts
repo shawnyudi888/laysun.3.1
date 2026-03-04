2fa74c737163777ef158d609f56589a73fff4b50
shawnyudi888 <9962463yudi@gmail.com>
2026-03-04 22:47:07 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
   publicDir: 'public',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
